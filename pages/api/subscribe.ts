1feb7ca13fbf83c555da71ada6b8d5ca2f847149
João Vitor Minosso <kallarari>
2022-04-28 17:00:45 -0300

    server 3

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -24,7 +24,7 @@ async function connectToDatabase(uri: string) {
 export default async (request: NowRequest, response: NowResponse) => {
   const { email } = request.body;
 
-  const db = await connectToDatabase(process.env.MONGODB_URI);
+  const db = await connectToDatabase(process.env.MONGODB_URI!);
 
   const collection = db.collection('subscribers');
 
